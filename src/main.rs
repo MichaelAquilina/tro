b2afa52ac655de9b150cfee5005765ce5d00385c
Michael Aquilina <michaelaquilina@gmail.com>
2018-12-27 23:20:25 +0000

    Implement basic TODO lister

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ extern crate serde_derive;
 extern crate reqwest;
 extern crate serde_json;
 
-use reqwest::Url;
+use reqwest::{Response, Url};
 use std::collections::HashMap;
 use std::env;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Board {
     name: String,
@@ -21,17 +21,40 @@ struct Board {
     label_names: HashMap<String, String>,
 }
 
+#[derive(Deserialize, Debug)]
+struct Label {
+    id: String,
+    name: String,
+    color: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Card {
+    id: String,
+    desc: String,
+    name: String,
+    url: String,
+    labels: Vec<Label>,
+}
+
+fn get_resource(url: &str, token: &str, key: &str) -> Response {
+    let url = Url::parse_with_params(url, &[("key", key), ("token", token)]).unwrap();
+
+    return reqwest::get(url).unwrap();
+}
+
 fn get_boards(token: &str, key: &str) -> Vec<Board> {
-    let url = Url::parse_with_params(
-        "https://api.trello.com/1/members/me/boards",
-        &[("key", &key), ("token", &token)],
-    )
-    .unwrap();
+    let mut resp = get_resource("https://api.trello.com/1/members/me/boards", token, key);
+    return resp.json().unwrap();
+}
 
-    let mut resp = reqwest::get(url).unwrap();
-    let text = resp.text().unwrap();
-
-    return serde_json::from_str(&text).unwrap();
+fn get_cards(board_id: &str, token: &str, key: &str) -> Vec<Card> {
+    let mut resp = get_resource(
+        &format!("https://api.trello.com/1/boards/{}/cards", board_id),
+        token,
+        key,
+    );
+    return resp.json().unwrap();
 }
 
 fn main() {
@@ -40,5 +63,16 @@ fn main() {
 
     let boards = get_boards(&token, &key);
 
-    println!("{:?}", boards[0]);
+    for (index, b) in boards.iter().enumerate() {
+        if b.name == "TODO" {
+            println!("{}: {}", index, b.name);
+            let cards = get_cards(&b.id, &token, &key);
+            for c in cards {
+                let labels: Vec<&String> = c.labels.iter().map(|l| &l.name).collect();
+
+                println!("{} {:?}", c.name, labels);
+            }
+            break;
+        }
+    }
 }
