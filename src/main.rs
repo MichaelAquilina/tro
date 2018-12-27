64f0ac3031935a2b13c4f4023994f6c6f0e6bc8b
Michael Aquilina <michaelaquilina@gmail.com>
2018-12-27 16:15:33 +0000

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,29 @@ extern crate serde_derive;
 extern crate reqwest;
 extern crate serde_json;
 
-use std::env;
 use reqwest::Url;
+use std::collections::HashMap;
+use std::env;
 
-
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Board {
     name: String,
     desc: String,
     url: String,
+    id: String,
+    starred: bool,
+    closed: bool,
+    subscribed: bool,
+    // TODO: Figure out how to map to a snake_case name
+    labelNames: HashMap<String, String>,
 }
-
 
 fn get_boards(token: &str, key: &str) -> Vec<Board> {
     let url = Url::parse_with_params(
         "https://api.trello.com/1/members/me/boards",
         &[("key", &key), ("token", &token)],
-    ).unwrap();
+    )
+    .unwrap();
 
     let mut resp = reqwest::get(url).unwrap();
     let text = resp.text().unwrap();
@@ -28,14 +34,11 @@ fn get_boards(token: &str, key: &str) -> Vec<Board> {
     return serde_json::from_str(&text).unwrap();
 }
 
-
 fn main() {
     let token = env::var("TRELLO_API_TOKEN").unwrap();
     let key = env::var("TRELLO_API_DEVELOPER_KEY").unwrap();
 
     let boards = get_boards(&token, &key);
 
-    for board in boards {
-        println!("{}", board.name);
-    }
+    println!("{:?}", boards[0]);
 }
