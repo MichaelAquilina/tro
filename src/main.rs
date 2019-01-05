d72c1f977334e3a148232f9752c002d5e64a7b92
Michael Aquilina <michaelaquilina@gmail.com>
2019-01-05 18:58:14 +0000

    Begin creating subcommands

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,38 @@ mod trello;
 #[macro_use]
 extern crate serde_derive;
 
+extern crate clap;
 extern crate console;
 
+use clap::{App, Arg, SubCommand};
 use console::StyledObject;
 use std::env;
+
+fn cards(board_name: &str, token: &str, key: &str) {
+    let boards = trello::get_boards(token, key);
+
+    for (index, b) in boards.iter().enumerate() {
+        if b.name.to_lowercase() == board_name {
+            println!("{}: {}", index, b.name);
+            let lists = trello::get_lists(&b.id, token, key);
+            for l in lists {
+                println!("{} ({})", l.name, l.id);
+            }
+
+            let cards = trello::get_cards(&b.id, token, key);
+            for c in cards {
+                let labels: Vec<StyledObject<&String>> = c
+                    .labels
+                    .iter()
+                    .map(|l| l.get_colored_name().bold())
+                    .collect();
+
+                println!("{} {:?}", c.name, labels);
+            }
+            break;
+        }
+    }
+}
 
 fn main() {
     let token = env::var("TRELLO_API_TOKEN");
@@ -20,27 +48,20 @@ fn main() {
     let token = token.unwrap();
     let key = key.unwrap();
 
-    let boards = trello::get_boards(&token, &key);
+    let app = App::new("trello-rst").subcommand(
+        SubCommand::with_name("cards")
+            .about("View cards in target Board")
+            .arg(
+                Arg::with_name("board")
+                    .help("Name of the board")
+                    .index(1)
+                    .required(true),
+            ),
+    );
+    let matches = app.get_matches();
 
-    for (index, b) in boards.iter().enumerate() {
-        if b.name == "TODO" {
-            println!("{}: {}", index, b.name);
-            let lists = trello::get_lists(&b.id, &token, &key);
-            for l in lists {
-                println!("{} ({})", l.name, l.id);
-            }
-
-            let cards = trello::get_cards(&b.id, &token, &key);
-            for c in cards {
-                let labels: Vec<StyledObject<&String>> = c
-                    .labels
-                    .iter()
-                    .map(|l| l.get_colored_name().bold())
-                    .collect();
-
-                println!("{} {:?}", c.name, labels);
-            }
-            break;
-        }
+    if let Some(matches) = matches.subcommand_matches("cards") {
+        let board_name = matches.value_of("board").unwrap().to_lowercase();
+        cards(&board_name, &token, &key);
     }
 }
