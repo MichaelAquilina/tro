3c13455bc482cf38f82f056d051a7f02e5e38bfd
Michael Aquilina <michaelaquilina@gmail.com>
2019-12-15 21:04:56 +0000

    Trello Client class

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod trello;
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello World");
     Ok(())
--- src/trello.rs
+++ src/trello.rs
@@ -1,24 +1,7 @@
 // TODO: Create generic Trello client that authenticates on its own
-use serde::Deserialize;
 use reqwest::Url;
+use serde::Deserialize;
 use std::error::Error;
-
-#[cfg(test)]
-use mockito;
-
-
-fn get_trello_url(path: &str, token: &str, key: &str) -> Result<Url, Box<dyn Error>> {
-    #[cfg(test)]
-    let host = mockito::server_url();
-
-    #[cfg(not(test))]
-    let host = "https://api.trello.com/";
-
-    return Ok(Url::parse_with_params(
-        &format!("{}{}", host, path),
-        &[("key", key), ("token", token)],
-    )?);
-}
 
 #[derive(Deserialize, Debug, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
@@ -28,47 +11,71 @@ pub struct Board {
     pub url: String,
 }
 
-impl Board {
-    pub fn get(board_id: &str, token: &str, key: &str) -> Result<Board, Box<dyn Error>> {
-        let url = get_trello_url(&format!("/1/boards/{}", board_id), token, key)?;
+pub struct Client {
+    pub host: String,
+    pub token: String,
+    pub key: String,
+}
+
+impl Client {
+    pub fn new(host: &str, token: &str, key: &str) -> Client {
+        Client {
+            host: String::from(host),
+            token: String::from(token),
+            key: String::from(key),
+        }
+    }
+
+    pub fn get_board(self, board_id: &str) -> Result<Board, Box<dyn Error>> {
+        let url = self.get_trello_url(&format!("/1/boards/{}", board_id))?;
 
         Ok(reqwest::get(url)?.error_for_status()?.json()?)
     }
+
+    fn get_trello_url(self, path: &str) -> Result<Url, Box<dyn Error>> {
+        return Ok(Url::parse_with_params(
+            &format!("{}{}", self.host, path),
+            &[("key", &self.key), ("token", &self.token)],
+        )?);
+    }
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use mockito::mock;
+    use mockito;
     use serde_json::json;
 
-
     #[test]
-    fn test_get_trello_url() -> Result<(), Box<dyn Error>>{
-        let result = get_trello_url("/foo/bar/", "some-secret-token", "some-key")?.to_string();
+    fn test_get_trello_url() -> Result<(), Box<dyn Error>> {
+        let client = Client::new("https://api.trello.com", "some-secret-token", "some-key");
+        let result = client.get_trello_url("/foo/bar/")?.to_string();
 
         // FIXME: this is not technically correct, should fix it
         // * parameter order should not make a difference
-        // * host name for non-test mode should somehow be tested
-        // * mockito server can probably change
-        assert_eq!(result, "http://127.0.0.1:1234/foo/bar/?key=some-key&token=some-secret-token");
+        assert_eq!(
+            result,
+            "https://api.trello.com/foo/bar/?key=some-key&token=some-secret-token"
+        );
         Ok(())
     }
 
     #[test]
     fn test_get_board() -> Result<(), Box<dyn Error>> {
-        let _m = mock("GET", "/1/boards/some-board-id?key=KEY&token=TOKEN")
+        let _m = mockito::mock("GET", "/1/boards/some-board-id?key=KEY&token=TOKEN")
             .with_status(200)
-            .with_body(json!({
-                "name": "My Favourite Board",
-                "id": "some-board-id",
-                "url": "https://trello.com/boards/some-board-id",
-            }).to_string())
+            .with_body(
+                json!({
+                    "name": "My Favourite Board",
+                    "id": "some-board-id",
+                    "url": "https://trello.com/boards/some-board-id",
+                })
+                .to_string(),
+            )
             .create();
 
-        let result = Board::get("some-board-id", "TOKEN", "KEY")?;
+        let result =
+            Client::new(&mockito::server_url(), "TOKEN", "KEY").get_board("some-board-id")?;
         let expected = Board {
             name: String::from("My Favourite Board"),
             id: String::from("some-board-id"),
