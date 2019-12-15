42efca96dc08e17cc0b31cc2082b87a6ca489f58
Michael Aquilina <michaelaquilina@gmail.com>
2019-12-15 21:23:42 +0000

    add get_all_boards

--- src/trello.rs
+++ src/trello.rs
@@ -26,6 +26,12 @@ impl Client {
         }
     }
 
+    pub fn get_all_boards(self) -> Result<Vec<Board>, Box<dyn Error>> {
+        let url = self.get_trello_url("/1/members/me/boards")?;
+
+        Ok(reqwest::get(url)?.error_for_status()?.json()?)
+    }
+
     pub fn get_board(self, board_id: &str) -> Result<Board, Box<dyn Error>> {
         let url = self.get_trello_url(&format!("/1/boards/{}", board_id))?;
 
@@ -57,6 +63,41 @@ mod tests {
             result,
             "https://api.trello.com/foo/bar/?key=some-key&token=some-secret-token"
         );
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_all_boards() -> Result<(), Box<dyn Error>> {
+        let _m = mockito::mock(
+            "GET",
+            "/1/members/me/boards?key=some-key&token=some-secret-token",
+        )
+        .with_status(200)
+        .with_body(
+            json!([
+                {"name": "TODO", "id": "abc-def", "url": "http://bit.ly/12"},
+                {"name": "foo", "id": "123-456", "url": ""},
+            ])
+            .to_string(),
+        )
+        .create();
+
+        let client = Client::new(&mockito::server_url(), "some-secret-token", "some-key");
+        let result = client.get_all_boards()?;
+        let expected: Vec<Board> = vec![
+            Board {
+                name: String::from("TODO"),
+                id: String::from("abc-def"),
+                url: String::from("http://bit.ly/12"),
+            },
+            Board {
+                name: String::from("foo"),
+                id: String::from("123-456"),
+                url: String::from(""),
+            },
+        ];
+
+        assert_eq!(result, expected);
         Ok(())
     }
 
