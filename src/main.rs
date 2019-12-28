383afec5f9b8c7c5f84ef1578b9d475ec1931fc9
Michael Aquilina <michaelaquilina@gmail.com>
2019-12-28 22:26:23 +0000

    Remove non-shortcut commands

--- src/main.rs
+++ src/main.rs
@@ -47,60 +47,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             (@arg card_name: !required "Card Name to retrieve")
             (@arg ignore_case: -i --("ignore-case") "Ignore case when searching")
         )
-        (@subcommand boards =>
-            (about: "Commands related to Trello boards")
-            (@subcommand create =>
-                (about: "Create a new board")
-                (@arg NAME: +required "Name of the board")
-            )
-            (@subcommand ls =>
-                (about: "List all available boards")
-            )
-            (@subcommand get =>
-                (about: "Get details for a specific board")
-                (@arg name: +takes_value "Specify board by name. Supports regex patterns.")
-                (@arg ignore_case: -i --("ignore-case") "Ignore case when searching by board name.")
-                (@subcommand close =>
-                    (about: "Close the board")
-                )
-                (@subcommand lists =>
-                    (about: "Interact with board lists")
-                    (@subcommand create =>
-                        (about: "Create a new list for this board")
-                        (@arg NAME: +required "Name of the list")
-                    )
-                    (@subcommand ls =>
-                        (about: "List all lists in this board")
-                    )
-                    (@subcommand get =>
-                        (about: "Get details for a specific list")
-                        (@arg name: +takes_value "Specify list by name. Supports regex patterns.")
-                        (@arg ignore_case: -i --("ignore-case") "Ignore case when searching by list name.")
-                        (@subcommand close =>
-                            (about: "Close the list")
-                        )
-                        (@subcommand cards =>
-                            (about: "Interact with list cards")
-                            (@subcommand create =>
-                                (about: "Create a new card for this list")
-                                (@arg NAME: +required "Name of the card")
-                            )
-                            (@subcommand ls =>
-                                (about: "List all cards in this list")
-                            )
-                            (@subcommand get =>
-                                (about: "Get details for a specific card")
-                                (@arg name: +takes_value "Specify card by name. Supports regex patterns.")
-                                (@arg ignore_case: -i --("ignore-case") "Ignore case when searching by card name.")
-                                (@subcommand close =>
-                                    (about: "Close the card")
-                                )
-                            )
-                        )
-                    )
-                )
-            )
-        )
     )
     .get_matches();
 
@@ -130,9 +76,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     debug!("Loaded configuration: {:?}", config);
 
-    if let Some(matches) = matches.subcommand_matches("boards") {
-        board_subcommand(&client, &matches)?;
-    } else if let Some(matches) = matches.subcommand_matches("show") {
+    if let Some(matches) = matches.subcommand_matches("show") {
         show_subcommand(&client, &matches)?;
     } else if let Some(matches) = matches.subcommand_matches("close") {
         close_subcommand(&client, &matches)?;
@@ -235,130 +179,6 @@ fn close_subcommand(client: &Client, matches: &ArgMatches) -> Result<(), Box<dyn
         Board::update(client, &board)?;
     }
 
-    Ok(())
-}
-
-fn card_subcommand(
-    client: &Client,
-    matches: &ArgMatches,
-    list_id: &str,
-) -> Result<(), Box<dyn Error>> {
-    debug!("Running card subcommand with {:?}", matches);
-
-    if let Some(matches) = matches.subcommand_matches("get") {
-        if let Some(card_name) = matches.value_of("name") {
-            let ignore_case = matches.is_present("ignore_case");
-            let cards = List::get_all_cards(&client, list_id)?;
-
-            if let Some(mut card) = get_object_by_name(cards, card_name, ignore_case)? {
-                if matches.subcommand_matches("close").is_some() {
-                    card.closed = true;
-                    Card::update(client, &card)?;
-                    println!("Closed card {} with id {}", card.name, card.id);
-                } else {
-                    println!("{}", card.render());
-                }
-            } else {
-                println!("Could not find a card with the name: {}", card_name);
-            }
-        } else {
-            println!("Must specify a filter to target card");
-        }
-    } else if matches.subcommand_matches("ls").is_some() {
-        let cards = List::get_all_cards(&client, list_id)?;
-        for card in cards {
-            println!("{}", card.name);
-        }
-    } else if let Some(matches) = matches.subcommand_matches("create") {
-        let card_name = matches.value_of("NAME").unwrap();
-        let card = Card::create(client, list_id, card_name)?;
-        println!("{:?}", card);
-    } else {
-        println!("{}", matches.usage());
-    }
-    Ok(())
-}
-
-fn list_subcommand(
-    client: &Client,
-    matches: &ArgMatches,
-    board_id: &str,
-) -> Result<(), Box<dyn Error>> {
-    debug!("Running list subcommand with {:?}", matches);
-
-    if let Some(matches) = matches.subcommand_matches("get") {
-        if let Some(list_name) = matches.value_of("name") {
-            let ignore_case = matches.is_present("ignore_case");
-            let lists = Board::get_all_lists(&client, board_id, true)?;
-
-            if let Some(mut list) = get_object_by_name(lists, list_name, ignore_case)? {
-                if let Some(matches) = matches.subcommand_matches("cards") {
-                    card_subcommand(client, matches, &list.id)?;
-                } else if matches.subcommand_matches("close").is_some() {
-                    list.closed = true;
-                    List::update(client, &list)?;
-                    println!("Closed list {} with id {}", list.name, list.id);
-                } else {
-                    println!("{}", list.render());
-                }
-            } else {
-                println!("Could not find a list with the name: {}", list_name);
-            }
-        } else {
-            println!("Must specify a filter to target list");
-        }
-    } else if matches.subcommand_matches("ls").is_some() {
-        let lists = Board::get_all_lists(client, board_id, true)?;
-        for list in lists {
-            println!("{}", list.name);
-        }
-    } else if let Some(matches) = matches.subcommand_matches("create") {
-        let list_name = matches.value_of("NAME").unwrap();
-        let list = List::create(client, board_id, list_name)?;
-        println!("{:?}", list);
-    } else {
-        println!("{}", matches.usage());
-    }
-
-    Ok(())
-}
-
-fn board_subcommand(client: &Client, matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    debug!("Running board subcommand with {:?}", matches);
-
-    if let Some(matches) = matches.subcommand_matches("get") {
-        if let Some(board_name) = matches.value_of("name") {
-            let ignore_case = matches.is_present("ignore_case");
-            let boards = Board::get_all(&client)?;
-
-            if let Some(mut board) = get_object_by_name(boards, board_name, ignore_case)? {
-                if let Some(matches) = matches.subcommand_matches("lists") {
-                    list_subcommand(client, matches, &board.id)?;
-                } else if matches.subcommand_matches("close").is_some() {
-                    board.closed = true;
-                    Board::update(client, &board)?;
-                    println!("Closed board {} with id {}", board.id, board.name);
-                } else {
-                    println!("{}", board.render());
-                }
-            } else {
-                println!("Could not find target board: '{}'", board_name);
-            }
-        } else {
-            println!("You must specify a filter");
-        }
-    } else if matches.subcommand_matches("ls").is_some() {
-        let boards = Board::get_all(&client)?;
-        for board in boards {
-            println!("{}", board.name);
-        }
-    } else if let Some(matches) = matches.subcommand_matches("create") {
-        let board_name = matches.value_of("NAME").unwrap();
-        let board = Board::create(&client, board_name)?;
-        println!("{:?}", board);
-    } else {
-        println!("{}", matches.usage());
-    }
     Ok(())
 }
 
