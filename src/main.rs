e380421a3ab81ebc27990bcde549726fbedd48cf
Michael Aquilina <michael.aquilina@snyk.io>
2020-03-11 12:07:55 +0100

    Add TODO for Copy on Write

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ struct TrelloConfig {
     key: String,
 }
 
+// TODO: Use Copy on Write instead of clone
 // TODO: Label card on creation
 // TODO: Enable truecolor support for labels
 // TODO: Move usage documentation to this file so that it can be doctested
